101ff0ab238c2dc18d58102feda60abe5b7eca1e
CastleRK <somedude2k@gmail.com>
2021-04-23 02:22:12 -0400

    Auto Resume Fix

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -23,7 +23,7 @@ class CarController():
                                                   CS.out.steeringTorque, CarControllerParams)
       self.steer_rate_limited = new_steer != apply_steer
 
-      if CS.out.standstill and frame % 20 == 0:
+      if CS.out.standstill and frame % 5 == 0:
         # Mazda Stop and Go requires a RES button (or gas) press if the car stops more than 3 seconds
         # Send Resume button at 5hz if we're engaged at standstill to support full stop and go!
         # TODO: improve the resume trigger logic by looking at actual radar data
